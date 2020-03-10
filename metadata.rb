397ecd2d1da7979784e11566d25ff530ebe20e5a
Larry Rosenman <ler@lerctr.org>
2020-03-10 18:45:40 -0500

    more fixes

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs and configures OpenLDAP (slapd) an open source implementation of LDAP.'
-version           '4.1.3'
+version           '4.1.4'
 
 %w(ubuntu debian freebsd redhat centos scientific oracle opensuse).each do |os|
   supports os
