b43626fc00376b89861cfcc77a28822c14999702
Larry Rosenman <ler@lerctr.org>
2020-10-26 11:11:36 -0500

    remove rebind-as-user

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs and configures OpenLDAP (slapd) an open source implementation of LDAP.'
-version           '4.1.8'
+version           '4.1.9'
 
 %w(ubuntu debian freebsd redhat centos scientific oracle opensuse).each do |os|
   supports os
